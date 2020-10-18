e84c9ebce6a7cf364b1b1147b25e8b00c232ff9c
Daniel Rendon <rendod99@gmail.com>
2020-10-18 17:20:13 -0500

    intitial comit, esablised sql connection and server conection

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+USE burgers_db;
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Bacon and Jalapeno Burger", false);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Chicken and Avacado Burger", false);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Mushroom and Swiss Cheese Burger", false);
