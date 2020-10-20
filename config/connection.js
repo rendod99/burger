fa77a6becbf3d07cc09e85d638bb4f21da484e99
Daniel Rendon <rendod99@gmail.com>
2020-10-20 01:12:20 -0500

    test index handlebars and burgers.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 var mysql = require("mysql");
 //require("dotenv").config();
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -8,7 +11,7 @@ var connection = mysql.createConnection({
   password: "Ripley2020!",
   database: "burgers_db"
 });
-
+};
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
