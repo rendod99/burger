75021704dd2787d065c1d59e6e0566ba7ceccd36
Daniel Rendon <rendod99@gmail.com>
2020-10-20 00:32:47 -0500

    updated model and routes

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
-require("dotenv").config();
+//require("dotenv").config();
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "MYSQLPASSWORD",
+  password: "Ripley2020!",
   database: "burgers_db"
 });
 
